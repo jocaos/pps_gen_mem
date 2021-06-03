f0f35be1b1546bf4f973f79c7b247c9754ea2cfd
jocaos <josias.carhuas.contractor@bbva.com>
2021-06-03 13:45:54 -0500

    first commit

---
+++ src/main/java/com/tesis2_pucp/pps_gen_mem/PpsGenMemApplication.java
@@ -0,0 +1,13 @@
+package com.tesis2_pucp.pps_gen_mem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpsGenMemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpsGenMemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tesis2_pucp/pps_gen_mem/PpsGenMemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tesis2_pucp.pps_gen_mem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpsGenMemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
